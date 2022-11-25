d7c48891caab73a05908e5faa5eb21a703ab02d7
Allan Monzon <greengamer.am@gmail.com>
2022-11-24 21:07:29 -0500

    commit tree 9b1e24be345c04f239f6d612e831d942235e819c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats=['Milo', 'Otis', 'Garfield'] 
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Broom")
+}
+function appendCat(name) {
+    const newArray = [...cats]
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name){
+    const newArray = [...cats]
+    newArray.unshift(name)
+   return newArray
+    
+}
+function removeLastCat(name){
+    const newArray = [...cats]
+    newArray.pop(name)
+   return newArray
+    
+}
+function removeFirstCat(name){
+    const newArray = [...cats]
+    newArray.shift(name)
+   return newArray
+    
+}
